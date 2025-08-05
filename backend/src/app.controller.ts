3547f9c0ee92086d70d6170dfa3d489a64d26736
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-08-05 14:28:39 +0300

    feat: reading env variables

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+console.log(process.env.GOOGLE_API_KEY);
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    console.log('Google API Key:', process.env.GOOGLE_API_KEY);
+
     return this.appService.getHello();
   }
 }
