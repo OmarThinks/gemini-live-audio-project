a0fdb1928d68c1add25e749b394fea6436f739c2
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-08-09 08:00:24 +0300

    feat: added the ability to connect and disconnect to websocket

--- with-socket-io/src/hooks/useWebSocketImplemntation.ts
+++ with-socket-io/src/hooks/useWebSocketImplemntation.ts
@@ -1,22 +1,47 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const useWebSocketImplementation = () => {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
   const socketRef = useRef<WebSocket | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
 
-  useEffect(() => {
+  const connectWebSocket = useCallback(() => {
     const ws = new WebSocket(
       "wss://generativelanguage.googleapis.com/ws/google.ai.generativelanguage.v1beta.GenerativeService.BidiGenerateContent"
     );
     socketRef.current = ws;
+    socketRef.current.onopen = () => {
+      console.debug("WebSocket connection opened");
+      setIsConnected(true);
+    };
+    socketRef.current.onmessage = (event) => {
+      console.debug("WebSocket message received:", event.data);
+    };
+    socketRef.current.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+    socketRef.current.onclose = (event) => {
+      console.debug("WebSocket connection closed:", event);
+      setIsConnected(false);
+    };
 
     return () => {
       socketRef.current?.close();
-      setSocket(null);
+      socketRef.current = null;
     };
   }, []);
 
-  return { socket: socketRef.current };
+  const disconnect = useCallback(() => {
+    socketRef.current?.close();
+    socketRef.current = null;
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      disconnect();
+    };
+  }, [connectWebSocket, disconnect]);
+
+  return { socket: socketRef.current, isConnected };
 };
 
 export { useWebSocketImplementation };
